d4d77872ee141c4be18612df58dd7daf1c80cb98
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-15 01:00:37 +0300

    Frontend:Adding Animation for input login form

--- linkedin_frontend/src/pages/Login.jsx
+++ linkedin_frontend/src/pages/Login.jsx
@@ -16,22 +16,40 @@ function Login() {
             Welcome to your professional community
           </h1>
           <form className="m-3 mx-auto " onSubmit={onsubmit}>
-            <input
-              type="email"
-              placeholder="Email"
-              className="p-3 my-2 w-3/4  border border-black outline-none focus:border-sky-600"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <input
-              type="password"
-              placeholder="password"
-              value={password}
-              className="p-3 my-2 w-3/4  border border-black outline-none focus:border-sky-600"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+            <div class="relative mx-auto">
+              <input
+                type="text"
+                id="floating_filled"
+                className="  peer
+                p-3 my-2 w-3/4  border border-black outline-none focus:border-sky-600
+                "
+                placeholder=" "
+              />
+              <label
+                for="floating_filled"
+                class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-6 right-96 z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+              >
+                Email
+              </label>
+            </div>
+      
+            <div class="relative mx-auto">
+              <input
+                type="text"
+                id="floating_filled"
+                className="  peer
+                p-3 my-2 w-3/4  border border-black outline-none focus:border-sky-600
+                "
+                placeholder=" "
+              />
+              <label
+                for="floating_filled"
+                class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-6 right-80
+                mr-9 z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+              >
+                Password
+              </label>
+            </div>
             <p className="text-center m-auto md:text-left w-3/4   text-sm">
               By clicking Agree & Join, you agree to the LinkedIn User
               Agreement, Privacy Policy, and Cookie Policy.
