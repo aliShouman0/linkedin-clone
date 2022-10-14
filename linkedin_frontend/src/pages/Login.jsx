0779625b9078106c0782f604135e49e402c92e19
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-15 00:14:37 +0300

    Frontend: Adding images  for login and adding general  tag

--- linkedin_frontend/src/pages/Login.jsx
+++ linkedin_frontend/src/pages/Login.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import { useState } from "react";
 import logo from "../assets/logo.png";
+import login_img from "../assets/login-img.svg";
 
 function Login() {
   return (
-    <>
-      <header>
-        <img src={logo} alt="logo" />
+    <div className="container">
+      <header className="h-20 w-28">
+        <img src={logo} alt="logo" className="h-14 w-28" />
       </header>
-      <h1 className="">Welcome to your professional community</h1>
-    </>
+      <main className="flex">
+        <div className="">
+          <h1 className="text-orange-900 text-lg font-bold tracking-wider">
+            Welcome to your professional community
+          </h1>
+          <form className="m-3" onSubmit={onsubmit}></form>
+        </div>
+        <div className="">
+          <img className="" src={login_img} alt="login_img" />
+        </div>
+      </main>
+    </div>
   );
 }
 
