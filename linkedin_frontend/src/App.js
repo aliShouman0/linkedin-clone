c7f7b7dd86a6d5f7f2ec5915caad32f103e3c410
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-14 23:44:26 +0300

    Frontend:Creating login page

--- linkedin_frontend/src/App.js
+++ linkedin_frontend/src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
-  return <div>
-
-  </div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path={"/"} element={<Login/>} />
+        <Route path={"/login"} element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ linkedin_frontend/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+function Login() {
+  return (
+    <>
+      <header>
+        <img src={logo} alt="logo" />
+      </header>
+      <h1 className="">Welcome to your professional community</h1>
+    </>
+  );
+}
+
+export default Login;
