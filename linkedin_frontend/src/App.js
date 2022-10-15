e8989b412a2743b0cf888938a0bf800f473401ee
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-15 15:15:11 +0300

    Frontend:Creating Home component and Start to implement nav component

--- linkedin_frontend/src/App.js
+++ linkedin_frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
@@ -9,6 +10,8 @@ function App() {
       <Route path={"/"} element={<Login/>} />
         <Route path={"/login"} element={<Login/>}/>
         <Route path={"/signup"} element={<Signup/>}/>
+        <Route path={"/home"} element={<Home/>}/>
+
 
       </Routes>
     </BrowserRouter>
---
+++ linkedin_frontend/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import home from "../assets/home.svg";
+import notification from "../assets/notification.svg";
+
+function Nav() {
+  return (
+    <nav className="w-screen h-auto bg-gray-200 flex ">
+     
+    </nav>
+  );
+}
+
+export default Nav;
