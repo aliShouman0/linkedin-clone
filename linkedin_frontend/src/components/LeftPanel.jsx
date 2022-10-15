3c5b373a5dfb41554f1966a8bb8610c243f44b0c
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-15 16:22:35 +0300

    Frontend:Styling left panel component

--- linkedin_frontend/src/components/LeftPanel.jsx
+++ linkedin_frontend/src/components/LeftPanel.jsx
@@ -3,7 +3,13 @@ import profile from "../assets/user.png";
 
 function LeftPanel() {
   return (
-   <></>
+    <div className="w-36  h-screen bg-gray-200 text-center">
+      <div className=" m-2">
+        <img src={profile} alt="Home" className="m-auto h-7 w-7" />
+        <p>userName</p>
+      </div>
+      <p>Degree</p>
+    </div>
   );
 }
 
