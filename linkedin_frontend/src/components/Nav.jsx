57bd3ee0b907ed06721611ca080b83a3b944a212
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-15 22:11:08 +0300

    Frontend:Adding applicant tab to navbar and make appear only if company login

--- linkedin_frontend/src/components/Nav.jsx
+++ linkedin_frontend/src/components/Nav.jsx
@@ -5,7 +5,7 @@ import notification from "../assets/notification.svg";
 import profile from "../assets/user.png";
 import search from "../assets/search.svg";
 
-function Nav({ active, setActive }) {
+function Nav({ active, setActive, isCompany }) {
   let notificationNb = 1;
   return (
     <nav className="w-full h-auto bg-gray-200 flex items-center justify-between ">
@@ -23,15 +23,16 @@ function Nav({ active, setActive }) {
 
         <button
           className={`text-sm mx-1 p-2 rounded-sm w-28  ${
-            active === "notification"
+            active === "notification" || active === "applicant"
               ? "bg-gray-300   border-b-2 border-black"
               : ""
           }`}
-          onClick={() => setActive("notification")}
+          onClick={() => setActive(isCompany ? "applicant" : "notification")}
         >
           <img src={notification} alt="Home" className="m-auto" />
-          Notifications &nbsp;
-          {notificationNb > 0 && (
+          {isCompany ? "Applicants" : "Notifications"}
+          &nbsp;
+          {!isCompany && notificationNb > 0 && (
             <small className="text-xs text-red-500 font-bold">
               {notificationNb}
             </small>
@@ -48,15 +49,20 @@ function Nav({ active, setActive }) {
           Profile
         </button>
       </div>
+
       <div className="flex ">
-        <input
-          type="text"
-          className="p-1.5 w-1/2 rounded-lg border border-black outline-none focus:border-sky-600 ml-auto"
-          placeholder="Search"
-        />
-        <button className="ml-1 mr-1">
-          <img src={search} alt="search" />
-        </button>
+        {!isCompany && (
+          <>
+            <input
+              type="text"
+              className="p-1.5 w-1/2 rounded-lg border border-black outline-none focus:border-sky-600 ml-auto"
+              placeholder="Search"
+            />
+            <button className="ml-1 mr-1">
+              <img src={search} alt="search" />
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
--- linkedin_frontend/src/pages/Home.jsx
+++ linkedin_frontend/src/pages/Home.jsx
@@ -3,20 +3,24 @@ import Nav from "../components/Nav";
 import { useState } from "react";
 import LeftPanel from "../components/LeftPanel";
 import Feed from "../components/Feed";
+import Applicant from "../components/Applicant";
 import Search from "../components/Search";
 import Notification from "../components/Notification";
 import Profile from "../components/Profile";
 
 function Home() {
   const [active, setActive] = useState("home");
+  const [isCompany, setIsCompany] = useState(true);
 
   return (
     <>
-      <Nav active={active} setActive={setActive} />
+      <Nav active={active} setActive={setActive} isCompany={isCompany}/>
       <main className="flex w-full  justify-between">
         <LeftPanel />
         {active === "home" ? <Feed /> : ""}
-        {active === "notification" ? <Notification /> : ""}
+        {active === "applicant" ? <Applicant /> : ""}
+
+        {active === "notification" ? <Notification  /> : ""}
         {active === "profile" ? <Profile /> : ""}
 
         <Search />
