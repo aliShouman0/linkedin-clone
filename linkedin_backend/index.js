ad56c31bdebd6e7b42c2873db1e2843a05feb8f8
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-18 12:47:11 +0300

    Backend:Fixing small bugs

--- linkedin_backend/index.js
+++ linkedin_backend/index.js
@@ -8,6 +8,13 @@ const userRoutes = require("./routes/users.route");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use("/job", jobsRoute);
 app.use("/auth", authRoutes);
